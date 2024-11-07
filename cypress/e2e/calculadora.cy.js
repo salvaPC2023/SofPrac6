f81ff4c8142d56f699796253286c9f5752055b18
SalvadorPatty <salvador.patty@ucb.edu.bo>
2024-11-07 06:54:26 -0400

    Cuarta prueba de cypress

--- cypress/e2e/calculadora.cy.js
+++ cypress/e2e/calculadora.cy.js
@@ -20,4 +20,11 @@ describe('Calculadora de Cadenas', () => {
     cy.get('#calcular-form').submit();
     cy.get('#resultado-div').should('contain', 'Suma de numeros de la cadena: 8');
     });
+
+    it('debería sumar varios números separados por coma', () => {
+        cy.get('#cadena').clear().type('4,3,2,1');
+        cy.get('#calcular-form').submit();
+        cy.get('#resultado-div').should('contain', 'Suma de numeros de la cadena: 10');
+    });
+    
   });
